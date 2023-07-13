3c8955666aa0bbbbde3ec253d93fe313cdbf6d66
AntoineCarlu <carluantoinepro@gmail.com>
2023-07-13 15:14:01 +0200

    header done

---
+++ Portfolio/src/pages/accueil.component.js
@@ -0,0 +1,30 @@
+import styles from '../styles/accueil.module.css';
+
+export default function Accueil() {
+  return (
+    <section className={styles.accueil}>
+      <div>
+        <h1>Enchanté, je m'appelle <span className={styles.red}>Antoine Carlu</span> !<br></br>Je suis un <span className={styles.red}>{presentJ}</span>,</h1>
+      </div>
+      <div>
+        <p>
+          Passioné par la programmation et le développement Web, <br></br>
+          mon interêt et ma curiosité m’ont fait découvrir ce domaine en 2022. <br></br>
+          Depuis, j’ai découvert de nombreux languages et méthodes de développement Web lors de formations professionnelles ou découvertes personnelles.
+        </p>
+      </div>
+      <div>
+        <img src="../../public/photo-cv.jpg" alt="Photo n&b"></img>
+      </div>
+    </section>
+  );
+}
+
+var presentJ = "Développeur Web";
+// setInterval(function() {
+//   presentJ = "Développeur Web";
+//   setTimeout(function() {
+//     presentJ = "Développeur";
+//   }, 2000);
+// }, 4000);
+  
--- Portfolio/src/pages/index.js
+++ Portfolio/src/pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import styles from '../styles/Home.module.css';
+import Header from './header.component';
+import Accueil from './accueil.component';
+import styles from '../styles/main.module.css';
 
 export default function Home() {
 	return (
@@ -13,57 +15,12 @@ export default function Home() {
 			</Head>
       <main className={styles.main}>
         {/* Header */}
-        <header className={styles.header}>
-          <a href=''>Home</a>
-          <div className={styles.headerList}>
-            <a href=''>Accueil</a>
-            <a href=''>Projets</a>
-            <a href=''>Profil</a>
-            <a href=''>Contact</a>
-            <div className={styles.burger} onClick={burgerClicked}>
-              <span className={styles.burger1} id="burger1"></span><span className={styles.burger2} id="burger2"></span><span className={styles.burger3} id="burger3"></span>
-            </div>
-          </div>
-        </header>
+        <Header />
         {/* Section Accueil */}
-        <section className={styles.accueil}>
-
-        </section>
+        <Accueil />
+        
       </main>
 		</>
 	);
 }
 
-
-
-//Event to display header list when click on the burger-menu icon
-var burgerCheck = false;
-function burgerClicked() {
-  //define elements
-  const Line1 = document.getElementById('burger1');
-  const Line2 = document.getElementById('burger2');
-  const Line3 = document.getElementById('burger3');
-  if (burgerCheck === false) {
-    //change visual of the burger icon
-    Line1.style.width = "8px";
-    Line1.style.borderColor = "rgba(248, 13, 65)";
-    Line2.style.width = "16px";
-    Line2.style.borderColor = "rgba(248, 13, 65)";
-    Line3.style.width = "24px";
-    Line3.style.borderColor = "rgba(248, 13, 65)";
-    burgerCheck = true;
-  }
-  else if (burgerCheck === true) {
-    //change visual of the burger icon
-    Line1.style.width = "30px";
-    Line1.style.borderColor = "white";
-    Line2.style.width = "30px";
-    Line2.style.borderColor = "white";
-    Line3.style.width = "30px";
-    Line3.style.borderColor = "white";
-    burgerCheck = false;
-  }
-  else {
-    throw new Error('Une erreur a été produite dans la liste de conditions.');
-  }
-}
