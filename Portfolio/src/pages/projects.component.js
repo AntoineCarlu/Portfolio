2737023efaac32ed9c4fa34cddc1ddc5759cb0ca
AntoineCarlu <carluantoinepro@gmail.com>
2023-07-18 11:46:34 +0200

    projects part

--- Portfolio/src/pages/projects.component.js
+++ Portfolio/src/pages/projects.component.js
@@ -4,7 +4,23 @@ import styles from '../styles/projects.module.css';
 export default function Projects() {
   return (
     <section className={styles.projects} id="Projects">
-      
+      <div className={styles.projectsFlex}>
+        <div className={styles.projectBox}>
+          <div><p>CV Web | Projet professionnel<br></br>HTML5 / CSS3 / JS</p></div>
+        </div>
+        <div className={styles.projectBox}>
+          <div><p>CV Web | Projet professionnel<br></br>HTML5 / CSS3 / JS</p></div>
+        </div>
+        <div className={styles.projectBox}>
+          <div><p>CV Web | Projet professionnel<br></br>HTML5 / CSS3 / JS</p></div>
+        </div>
+        <div className={styles.projectBox}>
+          <div><p>CV Web | Projet professionnel<br></br>HTML5 / CSS3 / JS</p></div>
+        </div>
+        <div className={styles.projectBox}>
+          <div><p>CV Web | Projet professionnel<br></br>HTML5 / CSS3 / JS</p></div>
+        </div>
+      </div>
     </section>
   );
 }
