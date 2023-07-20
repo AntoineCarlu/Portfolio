c8d0292058b2e47b76252a52809ec44d39e2226f
AntoineCarlu <carluantoinepro@gmail.com>
2023-07-20 14:12:59 +0200

    trying db connection

--- Portfolio/src/pages/projects.component.js
+++ Portfolio/src/pages/projects.component.js
@@ -1,25 +1,43 @@
 import styles from '../styles/projects.module.css';
+import React, { useEffect, useState } from 'react';
 
 
 export default function Projects() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    fetch('/api/projects.api')
+      .then((response) => response.json())
+      .then((data) => {
+        if (Array.isArray(data)) {
+          setProjects(data);
+        } else {
+          console.error('Error: Data is not an array', data);
+        }
+      })
+      .catch((error) => console.error('Error fetching data:', error));
+  }, []);
 
   return (
     <section className={styles.projects} id="Projects">
       <h1><u>Mes Projets</u></h1>
       {/*List of imported divs for each projects*/}
       <div className={styles.projectsFlex}>
-        <div className={styles.projectBox}>
+        {projects.map((project) => (
+          <div key={project.id} className={styles.projectBox}>
+            <a href={project.Project_link}><div className={styles.boxLink}><img src={project.Project_img} alt="" /></div></a>
+            <div className={styles.boxText}><p>{project.Project_descr}</p></div>
+          </div>
+        ))}
+
+        {/* <div className={styles.projectBox}>
           <a href=""><div className={styles.boxLink}></div></a>
           <div className={styles.boxText}><p>CV Web | Projet professionnel<br></br>HTML5 / CSS3 / JS</p></div>
         </div>
         <div className={styles.projectBox}>
           <a href=""><div className={styles.boxLink}></div></a>
           <div className={styles.boxText}><p>CV Web | Projet professionnel<br></br>HTML5 / CSS3 / JS</p></div>
-        </div>
-        <div className={styles.projectBox}>
-          <a href=""><div className={styles.boxLink}></div></a>
-          <div className={styles.boxText}><p>CV Web | Projet professionnel<br></br>HTML5 / CSS3 / JS</p></div>
-        </div>
+        </div> */}
       </div>
     </section>
   );
---
+++ Portfolio/src/utils/dbConnect.js
@@ -0,0 +1,21 @@
+import mysql from 'mysql2';
+
+const pool = mysql
+    .createPool({
+        host: 'db_portfolio',
+        port: 3307,
+        user: 'root',
+        password: 'root',
+        database: 'portfolio',
+    })
+    .promise();
+
+async function PortfolioDb() {
+  const data = await pool.query(
+      `
+      SELECT * FROM Projects
+      `
+  );
+  return data[0];
+}
+export default PortfolioDb;
