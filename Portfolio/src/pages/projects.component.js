3c2dadab31f258bcabb3a99d54c1ca8cc0acb606
AntoineCarlu <carluantoinepro@gmail.com>
2023-07-27 12:00:34 +0200

    uncomment

--- Portfolio/src/pages/projects.component.js
+++ Portfolio/src/pages/projects.component.js
@@ -4,20 +4,20 @@ import React, { useEffect, useState } from 'react';
 
 export default function Projects() {
 
-  // // Fetch datas from table projects in database
-  // const [projects, setProjects] = useState([]);
-  // useEffect(() => {
-  //   fetch('/api/projects.api')
-  //     .then((response) => response.json())
-  //     .then((data) => {
-  //       if (Array.isArray(data)) {
-  //         setProjects(data);
-  //       } else {
-  //         console.error('Error: Data is not an array', data);
-  //       }
-  //     })
-  //     .catch((error) => console.error('Error fetching data:', error));
-  // }, []);
+  // Fetch datas from table projects in database
+  const [projects, setProjects] = useState([]);
+  useEffect(() => {
+    fetch('/api/projects.api')
+      .then((response) => response.json())
+      .then((data) => {
+        if (Array.isArray(data)) {
+          setProjects(data);
+        } else {
+          console.error('Error: Data is not an array', data);
+        }
+      })
+      .catch((error) => console.error('Error fetching data:', error));
+  }, []);
 
   // Content of the section
   return (
@@ -25,12 +25,12 @@ export default function Projects() {
       <h1><u>Mes Projets</u></h1>
       {/*Imported divs for each projects*/}
       <div className={styles.projectsFlex}>
-      {/* {projects.map((project) => (
+      {projects.map((project) => (
         <div key={project.id} className={styles.projectBox}>
           <a href={project.Project_link} target='_blank'><div className={styles.boxLink}><img src={project.Project_img} alt="" /></div></a>
           <div className={styles.boxText}><p>{project.Project_descr}</p></div>
         </div>
-      ))} */}
+      ))}
       </div>
     </section>
   );
