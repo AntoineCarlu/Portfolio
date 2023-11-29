c4019f238ad60f3103d5817732ed554a00a46385
AntoineCarlu <carluantoinepro@gmail.com>
2023-11-29 09:57:36 +0100

    feature: adding CRUD functions for "Projects" table

---
+++ Portfolio/src/app/api/projects/[id]/route.js
@@ -0,0 +1,20 @@
+import connectMongoDB from "@/libs/dbConnect";
+import Project from "@/models/project";
+import { NextResponse } from "next/server";
+
+// Function to UPDATE "Projects" elements in the MongoDB database
+export async function PUT(request, { params }) {
+  const { id } = params;
+  const { NewLink: project_link, NewImg: project_img, NewDescription: project_descr } = await request.json();
+  await connectMongoDB();
+  await Project.findByIdAndUpdate(id, { project_link, project_img, project_descr });
+  return NextResponse.json({ message: "Project updated" }, { status: 200 });
+}
+
+// Function to find specific "Project" element with id in the MongoDB database
+export async function GET(request, { params }) {
+  const { id } = params;
+  await connectMongoDB();
+  const project = await Project.findOne({ _id: id });
+  return NextResponse.json({ project }, { status: 200 });
+}
---
+++ Portfolio/src/app/api/projects/route.js
@@ -0,0 +1,26 @@
+import connectMongoDB from "@/libs/dbConnect";
+import Project from "@/models/project";
+import { NextResponse } from "next/server";
+
+// Function to POST datas from "Projects" schemas, into the MongoDB database
+export async function POST(request) {
+  const { project_link, project_img, project_descr } = await request.json();
+  await connectMongoDB();
+  await Project.create({ project_link, project_img, project_descr });
+  return NextResponse.json({ message: "Project Created" }, { status: 201 });
+}
+
+// Function to GET "Projects" datas elements from the MongoDB database
+export async function GET() {
+  await connectMongoDB();
+  const projects = await Project.find();
+  return NextResponse.json({ projects });
+}
+
+// Function to DELETE "Projects" elements from the MongoDB database
+export async function DELETE(request) {
+  const id = request.nextUrl.searchParams.get("id");
+  await connectMongoDB;
+  await Project.findByIdAndDelete(id);
+  return NextResponse.json({ message: "Project deleted."}, { status: 200 });
+}
