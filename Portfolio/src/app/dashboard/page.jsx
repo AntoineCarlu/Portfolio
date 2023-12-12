22343ed26ff7ca6bac7f4122cf02104d7d35a1c5
AntoineCarlu <carluantoinepro@gmail.com>
2023-12-12 16:35:33 +0100

    feature: added component to create a project in the admin dashboard

--- Portfolio/src/app/dashboard/page.jsx
+++ Portfolio/src/app/dashboard/page.jsx
@@ -4,9 +4,11 @@ import { isAuthenticated } from '@/libs/auth';
 import { redirect } from 'next/navigation';
 import { useLayoutEffect } from 'react';
 import styles from '@/app/page.module.css';
+import AddProject from '@/components/dashboard.components/AddProject.component';
 
 export default function Dashboard() {
 
+  // Function to protect the route to being access by unauthorized user
   useLayoutEffect(() => { 
     const isAuth = isAuthenticated;
     if(!isAuth) {
@@ -16,7 +18,7 @@ export default function Dashboard() {
 
   return (
     <main className={styles.main}>
-
+      <AddProject />
     </main>
   );
 }
---
+++ Portfolio/src/components/dashboard.components/AddProject.component.jsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { useState } from "react";
+
+export default function AddProject() {
+
+  const [project_link, setProject_link] = useState('');
+  const [project_img, setProject_img] = useState('');
+  const [project_descr, setProject_descr] = useState('');
+
+  // Function to add news "Projects" with form element
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!project_link || !project_img || !project_descr) {
+      alert('Please fill in all fields.');
+      return;
+    }
+
+    try {
+      const res = await fetch('http://localhost:3000/api/projects', {
+        method: 'POST',
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify({ project_link, project_img, project_descr }),
+      });
+
+      if (res.ok) {
+        console.log('Project created successfully.');
+      } else {
+        throw new Error('Failed to create a new project.');
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={(e) => setProject_link(e.target.value)}
+          value={project_link}
+          type="text" 
+          placeholder='Project link' 
+        />
+
+        <input
+          onChange={(e) => setProject_img(e.target.value)}
+          value={project_img}
+          type="text" 
+          placeholder='Project image link' 
+        />
+
+        <input
+          onChange={(e) => setProject_descr(e.target.value)}
+          value={project_descr}
+          type="text" 
+          placeholder='Project description' 
+        />
+
+        <button type="submit">Add Project</button>
+      </form>
+    </div>
+  );
+
+}
