52166b3cfa275c11507df288ba5a2414a12a87bd
AntoineCarlu <carluantoinepro@gmail.com>
2024-02-14 15:12:17 +0100

    feature: fade animation when projects divs appears in user viewport

--- Portfolio/src/components/projects/Projects.component.jsx
+++ Portfolio/src/components/projects/Projects.component.jsx
@@ -1,3 +1,6 @@
+"use client"
+
+import { useEffect, useState } from 'react';
 import Link from 'next/link';
 import styles from './projects.module.css'
 
@@ -18,8 +21,43 @@ const getProjects = async () => {
   }
 }
 
-export default async function ProjectsList() {
-  const { projects } = await getProjects();
+export default function ProjectsList() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    const fetchProjects = async () => {
+      const data = await getProjects();
+      setProjects(data.projects || []);
+    };
+
+    fetchProjects();
+  }, []);
+
+  //Function to have a fade animation when projects divs appears in user viewport
+  useEffect(() => {
+    const projectsAnimation = () => {
+      const elements = document.querySelectorAll('.fade-in');
+      elements.forEach((element) => {
+        //calculation of when the divs appears in user viewport
+        const elementTop = element.getBoundingClientRect().top;
+        const viewportHeight = window.innerHeight;
+        const quarterViewport = viewportHeight / 4;
+        const isVisible = elementTop <= viewportHeight - quarterViewport;
+
+        //change the opacity when in user viewport for animation
+        if (isVisible) {
+          element.style.opacity = 1;
+        } else {
+          element.style.opacity = 0;
+        }
+      });
+    };
+
+    window.addEventListener('scroll', projectsAnimation);
+    return () => {
+      window.removeEventListener('scroll', projectsAnimation);
+    };
+  }, []);
 
   // Page content
   return (
@@ -28,7 +66,7 @@ export default async function ProjectsList() {
       {/*Imported divs for each projects*/}
       <div className={styles.projectsFlex}>
       {projects.map((project) => (
-        <div key={project.id} className={styles.projectBox}>
+        <div key={project.id} className={`${styles.projectBox} fade-in`}>
           <Link href={project.project_link} target='_blank'>
             <div className={styles.box_link}>
               <img src={project.project_img} alt="" />
