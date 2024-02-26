811ee53bc822d03b01e3ac0ce26688577425eb1a
AntoineCarlu <carluantoinepro@gmail.com>
2024-02-26 14:53:49 +0100

    feature: adding the Create divs for education and experience in the admin dashboard

--- Portfolio/src/app/dashboard/page.jsx
+++ Portfolio/src/app/dashboard/page.jsx
@@ -8,6 +8,8 @@ import AddProject from '@/components/dashboard.components/AddProject.component';
 import ProjectsList from '@/components/dashboard.components/FetchProjects.component';
 import AddSkill from '@/components/dashboard.components/AddSkill.component';
 import SkillsList from '@/components/dashboard.components/FetchSkills.component';
+import AddEducation from '@/components/dashboard.components/AddEducation.component';
+import AddExperience from '@/components/dashboard.components/AddExperience.component';
 
 export default function Dashboard() {
   const [renderProjectsList, setRenderProjectsList] = useState(false);
@@ -51,6 +53,16 @@ export default function Dashboard() {
               <span onClick={() => setRenderSkillsList(!renderSkillsList)}>Cliquez pour voir la liste des compétences</span>
               {renderSkillsList ? <SkillsList /> : ""}
             </div>
+          </div>
+
+          <div className={styles.dashboard_div}>
+            <h2>Education | Create</h2>
+            <AddEducation />
+          </div>
+
+          <div className={styles.dashboard_div}>
+            <h2>Experience | Create</h2>
+            <AddExperience />
           </div>
         </div>
 
