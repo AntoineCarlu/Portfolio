375a5ac53d277d568c107b3bbaf7f620c607c081
AntoineCarlu <carluantoinepro@gmail.com>
2024-03-11 14:29:42 +0100

    feature: working on the form of Contact and css designs

--- Portfolio/src/components/contact/Contact.component.jsx
+++ Portfolio/src/components/contact/Contact.component.jsx
@@ -3,7 +3,17 @@ import styles from './contact.module.css';
 export default function Contact() {
   return (
     <div className={styles.contact} id="Contact">
-      test
+      <h1><u>Contactez moi</u></h1>
+      <form className={styles.contactForm}>
+        <div className={styles.twoInputs}>
+          <input type='text' placeholder='Nom'></input>
+          <input type='text' placeholder='Numero'></input>
+        </div>
+        <input type='text' placeholder='Email'></input>
+        <input type='text' placeholder='Sujet/Titre'></input>
+        <input type='text' placeholder='Message'></input>
+        <button type='submit'>ENVOYER</button>
+      </form>
     </div>
   );
 }
